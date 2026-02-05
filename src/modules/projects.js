ced4042a1bc50e0a4e5a486c7efd944575a0485a
Jacob Bilby <158042318+jbprojects95@users.noreply.github.com>
2026-02-05 11:17:50 +0000

    setting up modules for todo items

---
+++ src/modules/projects.js
@@ -0,0 +1,5 @@
+export class Projects {
+  constructor(name) {
+    this.name = name;
+  }
+}
---
+++ src/modules/todo.js
@@ -0,0 +1,10 @@
+export class TodoItem {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.completed = false;
+    this.notes = "";
+  }
+}
