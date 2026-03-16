ec5dbd4b04ec4393820fd8260aa5cf20b5d66468
Jacob Bilby <158042318+jbprojects95@users.noreply.github.com>
2026-03-16 12:01:11 +0000

    added form to create todo

--- src/pages/createTodo.js
+++ src/pages/createTodo.js
@@ -4,7 +4,25 @@ export function loadTodo(container) {
   container.innerHTML = `
       <div class="wrapper">
       <div class="container">
-          <h1>Todo Item Example: Empty dishwasher</h1>
+          <form action="" method="get" class="todo-form">
+  <div class="form-row">
+    <label for="project">Project:</label>
+    <select name="project" id="selectProject">
+      <option value="work">Work</option>
+      <option value="home">Home</option>
+      <option value="misc">Misc</option>
+    </select>
+  </div>
+
+  <div class="form-row">
+    <label for="todo-item">Todo:</label>
+    <input type="text" id="todoItemText" name="todo-item" />
+  </div>
+
+        <div class="form-row">
+        <button>Submit</button>
+      </div>
+</form>
 
         </div>
   </div>
